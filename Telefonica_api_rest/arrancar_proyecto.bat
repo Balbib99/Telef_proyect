03bb4f2fb17ccc79762960012394684216ab9598
Balbib99 <balbib99@gmail.com>
2023-12-03 19:54:48 +0100

    Cambios en los sistemas de arranque y cierre de proyecto

--- Telefonica_api_rest/arrancar_proyecto.bat
+++ Telefonica_api_rest/arrancar_proyecto.bat
@@ -1,15 +1,18 @@
-@REM Arrancamos el cmd y dentro ejecutamos el servidor de nuestra base de datos
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe"
+@echo off
 
-@REM Arrancamos el cmd y dentro nos dirigimos a la ruta de nuestro proyecto de api y lo ejecutamos
-start cmd.exe /k "cd C:\Users\balbi\OneDrive\Escritorio\Telef_proyect\Telefonica_api_rest && npm start"
+:: Arrancamos el cmd y dentro ejecutamos el servidor de nuestra base de datos
+start "MongoDB" cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe"
 
-@REM Arrancamos el cmd y dentro nos dirigimos a la ruta de nuestro proyecto frontend y lo ejecutamos
-start cmd.exe /k "cd C:\Users\balbi\OneDrive\Escritorio\Telef_proyect\Telefonica\telefonica && npm run dev"
+:: Espera un momento para que MongoDB se inicie
+timeout /t 5 > nul
 
-@REM Arrancamos el cmd y dentro ejecutamos el siguiente comando que accede a dicha url en el navegador
+:: Arrancamos el cmd y dentro nos dirigimos a la ruta de nuestro proyecto de API y lo ejecutamos
+start "API" cmd.exe /k "cd C:\Users\balbi\OneDrive\Escritorio\Telef_proyect\Telefonica_api_rest && npm start"
 
-:: Abre una URL en el navegador
+:: Arrancamos el cmd y dentro nos dirigimos a la ruta de nuestro proyecto frontend y lo ejecutamos
+start "Frontend" cmd.exe /k "cd C:\Users\balbi\OneDrive\Escritorio\Telef_proyect\Telefonica\telefonica && npm run dev"
+
+:: Arrancamos el cmd y dentro ejecutamos el siguiente comando que accede a dicha URL en el navegador
 start "" "http://localhost:5173/"
 
 :: Espera a que se cierre la ventana del navegador
--- Telefonica_api_rest/cerrar_proyecto.bat
+++ Telefonica_api_rest/cerrar_proyecto.bat
@@ -6,7 +6,13 @@ taskkill /IM mongod.exe /F
 :: Detener el servidor de API
 taskkill /IM node.exe /F
 
-:: No es posible detener la solicitud curl directamente desde este archivo .bat
+:: Cerrar las ventanas CMD específicas
+taskkill /FI "WINDOWTITLE eq MongoDB" /F
+taskkill /FI "WINDOWTITLE eq API" /F
+taskkill /FI "WINDOWTITLE eq Frontend" /F
+
+:: Intentar cerrar la ventana de MongoDB por el título original o parte del texto que la identifique
+taskkill /FI "WINDOWTITLE eq mongod.exe" /F
 
 echo Procesos detenidos
 pause
